40b6dd343c4896b8a6cfa43b8aac1f7009e0e03e
Nkateko00 <tekonku@icloud.com>
2020-08-02 12:59:06 +0200

    factory done

--- factory/registration.js
+++ factory/registration.js
@@ -2,27 +2,37 @@ function regNumbers(registration) {
 
     var myList = registration || [];
 
-    function regList(regPlate) {
-        var newList = [];
-        for (var i = 0; i < myList.length; i++) {
-            var current = myList[i];
-            if (current.startsWith("CA")) {
-                newList.push(current);
-                //always push to empty list/array
+    function selectedTown(regPlate) {
+
+        if (regPlate = "all") {
+            return myList;
+            //always push to empty list/array
+        }
+        else {
+            var newList = [];
+            for (var i = 0; i < myList.length; i++) {
+                var current = myList[i];
+                if (current.startsWith(regPlate)) {
+                    newList.push(current);
+                }
             }
-            if (current.startsWith("CK")) {
-                newList.push(current);
-            }
-            if (current.startsWith("CY")) {
-                newList.push(current);
-            }
+            return newList;
+            // for-loop is filtering, List stores & retuns new data
+
         }
     }
     function getRegistration(regNum) {
-        //regex
-        if(regNum !== ""){
-            var regularExpression
-            if()
+        // regex  to check if textbox includes the regEx
+        // use .test() to  test the regNumber & the regEx
+        //list doesnt iclude same number push
+        if (regNum !== "") {
+            var regularExpression = /C[YJA] \d{3}[\s]? \d{3}[\s]?/g
+            if (regularExpression.test(regNum) && !myList.includes(regNum)) {
+                myList.push(regNum)
+            }
+        }
+        else {
+            return false;
         }
         myList.push(regNum);
     }
@@ -33,7 +43,7 @@ function regNumbers(registration) {
     }
 
     return {
-        regList,
+        selectedTown,
         regDisplay,
         getRegistration
 
